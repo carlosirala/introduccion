da6b972bbb2a42c72afa306a872b3c6af6409af5
carlosirala <32582523+carlosirala@users.noreply.github.com>
2017-11-30 01:01:20 -0600

    .
    .

---
+++ Practicas/P4/ejercicio4.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+float v,i,r,t;
+int main(){
+	printf("Bienvenido, este programa calcula la ley de ohm\n");
+	printf("Introduzca el numero de lo que desea calcular\n1. Voltaje\t2. Intesidad de corriente\t3. Resistencia\n")
+	system("pause");
+}
