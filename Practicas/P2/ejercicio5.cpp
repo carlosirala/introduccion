3885490ba52f61c198db29ae256bf40697a1fb97
carlosirala <32582523+carlosirala@users.noreply.github.com>
2017-11-30 09:27:09 -0600

    .
    .

--- Practicas/P2/ejercicio5.cpp
+++ Practicas/P2/ejercicio5.cpp
@@ -3,7 +3,7 @@
 int main(){
 	float n1,n2,total;
 	char operador;
-	printf("introduzca operador (+,-)\n");
+	printf("introduzca operador (+,-,*,/)\n");
 	scanf("%c",&operador);
 	printf("Bienvenido, introduzca el primer numero\n");
 	scanf("%f",&n1);
@@ -14,6 +14,12 @@ int main(){
 	}
 	if(operador=='-'){
 		total=n1-n2;
+	}
+	if(operador=='*'){
+		total=n1*n2;
+	}
+	if(operador=='/'){
+		total=n1/n2;
 	}
 	printf("El resultado es:\n%f\n",total);
 	system("pause");
